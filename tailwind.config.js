ec466db641f167e418837b0641b0932a0102f593
Eva <orakle@tutamail.com>
2023-10-17 21:37:59 +0200

    Squelette index.html, correction mineure sur tourisme.html, corrections header.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "src/*.{html}",
+      "src/*.html",
       "src/scripts/*.{ts,js}",
       "./templates/*.html"
   ],
